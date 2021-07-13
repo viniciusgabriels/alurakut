39eb62b7107e2c5866d6cebc48216b260a223920
Vinícius Gabriel <viniciusgabriel.rs@hotmail.com>
2021-07-13 15:07:01 -0300

    first commit

---
+++ src/components/Box/index.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const Box = styled.div `
+  background: #ffffff;
+  border-radius: 8px;
+  padding: 16px;
+
+  margin-bottom: 10px;
+  .boxLink {
+    font-size: 14px;
+    font-weight: 800;
+    color: #2E7BB4;
+    text-decoration: none;
+  }
+  .title {
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 20px;    
+  }
+  .subTitle {
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 20px;    
+  }
+  .smallTitle {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    color: #333;    
+  }
+  hr {
+    margin-top: 12px;
+    margin-bottom: 8px;
+    border-color: transparent;
+    border-bottom-color: #ECF2FA;
+  }
+  input {
+    width: 100%;
+    background-color: #F4F4F4;
+    color: #333;
+    border: 0;
+    padding: 14px 16px;
+    margin-bottom: 14px;
+    border-radius: 10000px;
+    ::placeholder {
+      color: #333333;
+      opacity: 1;
+    }
+  }
+  button {
+    border: 0;
+    padding: 8px 12px;
+    color: #FFF;
+    border-radius: 10000px;
+    background-color: #6F92BB;
+  }
+`;
+
+export default Box;
